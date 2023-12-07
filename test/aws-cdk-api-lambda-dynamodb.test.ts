b97a41079b115e58811b28a5d05f381701b18b73
nirgluzman <nir.gluzman@gmail.com>
2023-12-07 09:23:38 +0100

    Initial commit

---
+++ test/aws-cdk-api-lambda-dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkApiLambdaDynamodb from '../lib/aws-cdk-api-lambda-dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-api-lambda-dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkApiLambdaDynamodb.AwsCdkApiLambdaDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
