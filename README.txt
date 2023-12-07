fb0713a4db6e4955d219f8a58d2a550c9c7a1c76
nirgluzman <nir.gluzman@gmail.com>
2023-12-07 10:41:31 +0100

    add S3 bucket with public read access

--- README.txt
+++ README.txt
@@ -10,3 +10,8 @@ cdk init --list
 
 Template for a CDK Application:
 cdk init app --language typescript
+
+
+Allow public read access to S3 bucket
+https://repost.aws/questions/QU_J7Q6ULvQ8iJac5mBQ2paw/failure-to-create-an-s3-bucket-with-publicreadaccess-true-config
+https://aws.amazon.com/blogs/aws/amazon-s3-block-public-access-another-layer-of-protection-for-your-accounts-and-buckets/
--- lib/aws-cdk-api-lambda-dynamodb-stack.ts
+++ lib/aws-cdk-api-lambda-dynamodb-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class AwsCdkApiLambdaDynamodbStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkApiLambdaDynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		// create an S3 bucket with public access
+		const bucket = new s3.Bucket(this, 'myBucket', {
+			autoDeleteObjects: true, // automatically delete objects when the bucket is deleted.
+			removalPolicy: cdk.RemovalPolicy.DESTROY, // resource should be deleted when the stack that created it is deleted.
+			versioned: false, // disable versioning for the bucket.
+			// allow public read access to the bucket.
+			publicReadAccess: true,
+			blockPublicAccess: {
+				blockPublicAcls: false,
+				blockPublicPolicy: false,
+				ignorePublicAcls: false,
+				restrictPublicBuckets: false,
+			},
+		});
+	}
 }
